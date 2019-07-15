3834d8b2cf66443cf0c14697c6b3c34ddfb7c367
Evan Kysley <evan@kysley.com>
2019-07-15 14:06:20 -0400

    feat: accountById helper

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import * as jwt from 'jsonwebtoken'
+
+import {Context} from '../types'
+
+export function getAccountId(ctx: Context) {
+  const Authorization = ctx.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const {accountId} = jwt.verify(token, process.env.APP_SECRET) as {
+      accountId: string
+    }
+    return accountId
+  }
+
+  throw new Error('not auth')
+}

a1f333d3bc65cb1fd4f8d3c4802b2b1f6b0ea2a9
Evan Kysley <evan@kysley.com>
2019-07-15 16:00:30 -0400

    feat: (query) me

--- src/resolvers/queries/Account.ts
+++ src/resolvers/queries/Account.ts
@@ -1,8 +1,13 @@
 import {Context} from '../../types'
 import {AccountFragment} from '../fragments/AccountFragment'
+import {getAccountId} from '../../utils'
 
 export const AccountQueries = {
   account: async (_, {id}, ctx: Context) => {
     return await ctx.prisma.account({id}).$fragment(AccountFragment)
+  },
+  me: async (_, __, ctx: Context) => {
+    const userId = getAccountId(ctx)
+    return await ctx.prisma.account({id: userId}).$fragment(AccountFragment)
   },
 }

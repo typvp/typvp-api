98398c96d95de0dcc70a7123c4523fa226fad68a
Evan Kysley <evan@kysley.com>
2019-07-15 14:04:59 -0400

    feat: add graphql-shield

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import {GraphQLServer} from 'graphql-yoga'
 import * as dotenv from 'dotenv'
+import {rule, shield, allow} from 'graphql-shield'
 
 import resolvers from './resolvers'
 import {prisma} from './generated/prisma-client'
+import {getAccountId} from './utils'
 
 dotenv.config()
 
+const isAuthenticated = rule()(async (parent, args, ctx, info) => {
+  return getAccountId(ctx) !== null
+})
+
+const permissions = shield({
+  Query: {
+    account: allow,
+  },
+  Mutation: {
+    addResults: isAuthenticated,
+  },
+})
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers: resolvers,
+  resolvers,
+  middlewares: [permissions],
   context: request => ({
     ...request,
     prisma,

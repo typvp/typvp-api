1e13b67a13c9b2b035e63f203d5b25306eca50e7
Evan Kysley <evan@kysley.com>
2019-07-24 23:13:59 -0400

    feat: attempt to add seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import {prisma} from '../src/generated/prisma-client'
+
+async function main() {
+  await prisma.createAccount({
+    email: 'evam@kysley.com',
+    username: 'Evan',
+    password: '$2b$10$dqyYw5XovLjpmkYNiRDEWuwKaRAvLaG45fnXE5b3KTccKZcRPka2m', // "secret42"
+    role: 'ADMIN',
+  })
+  await prisma.createAccount({
+    email: 'bob@prisma.io',
+    username: 'Bob',
+    password: '$2b$10$o6KioO.taArzboM44Ig85O3ZFZYZpR3XD7mI8T29eP4znU/.xyJbW', // "secret43"
+    role: 'USER',
+  })
+  await prisma.createRoom({
+    roomState: 'IDLE',
+    race: {
+      create: {
+        wordSet: 'Sample',
+        raceState: 'AWAITING',
+      },
+    },
+  })
+}

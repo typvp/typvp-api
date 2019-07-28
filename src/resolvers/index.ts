f9509e57295c3858c04b9dffe02ef5cbf5fcf793
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-07-28 19:00:25 -0400

    added subscriptions to index eport file

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,15 +3,23 @@ import {SingleplayerMutations} from './mutations/Singleplayer'
 import {RaceMutations} from './mutations/Race'
 import {AccountQueries} from './queries/Account'
 import {TestQueries} from './queries/Test'
+import {RaceSubscriptions} from './subscriptions/Race'
+import {RoomMutations} from './mutations/Room'
+import {RoomSubscriptions} from './subscriptions/Room'
 
 export default {
   Mutation: {
     ...Auth,
     ...SingleplayerMutations,
     ...RaceMutations,
+    ...RoomMutations,
   },
   Query: {
     ...AccountQueries,
     ...TestQueries,
+  },
+  Subscription: {
+    ...RaceSubscriptions,
+    ...RoomSubscriptions,
   },
 }

d377828c26030b729221c543fbd7842a20d9434f
Evan Kysley <evan@kysley.com>
2019-08-20 12:21:41 -0400

    chore: update seeds to create Trials

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv'
-import {rword} from 'rword'
+import {generate} from '@typvp/gen'
 dotenv.config()
 
 import {prisma} from '../src/generated/prisma-client'
@@ -18,22 +18,82 @@ async function main() {
     role: 'USER',
   })
 
-  const wordSet1 = (rword.generate(250, {length: '3-8'}) as string[]).join('|')
+  const normalSet1 = generate(250, {
+    minLength: 3,
+    maxLength: 8,
+    join: '|',
+  }) as string
   await prisma.createTrial({
-    name: 'typvp Seeded Trail 1',
-    wordSet: wordSet1,
+    name: 'typvp Standard Trail 1',
+    wordSet: normalSet1,
+    minWordLength: 3,
+    maxWordLength: 8,
+    difficulty: 'NORMAL',
   })
 
-  const wordSet2 = (rword.generate(250, {length: '3-8'}) as string[]).join('|')
+  const normalSet2 = generate(250, {
+    minLength: 3,
+    maxLength: 8,
+    join: '|',
+  }) as string
   await prisma.createTrial({
-    name: 'typvp Seeded Trail 2',
-    wordSet: wordSet2,
+    name: 'typvp Standard Trail 2',
+    wordSet: normalSet2,
+    minWordLength: 3,
+    maxWordLength: 8,
+    difficulty: 'NORMAL',
   })
 
-  const wordSet3 = (rword.generate(250, {length: '3-8'}) as string[]).join('|')
+  const normalSet3 = generate(250, {
+    minLength: 3,
+    maxLength: 8,
+    join: '|',
+  }) as string
   await prisma.createTrial({
-    name: 'typvp Seeded Trail 3',
-    wordSet: wordSet3,
+    name: 'typvp Standard Trail 3',
+    wordSet: normalSet3,
+    minWordLength: 3,
+    maxWordLength: 8,
+    difficulty: 'NORMAL',
+  })
+
+  const easySet1 = generate(250, {
+    minLength: 3,
+    maxLength: 5,
+    join: '|',
+  }) as string
+  await prisma.createTrial({
+    name: 'typvp Easy Trial 1',
+    wordSet: easySet1,
+    minWordLength: 3,
+    maxWordLength: 5,
+    difficulty: 'EASY',
+  })
+
+  const mediumSet1 = generate(250, {
+    minLength: 4,
+    maxLength: 8,
+    join: '|',
+  }) as string
+  await prisma.createTrial({
+    name: 'typvp Medium Trial 1',
+    wordSet: mediumSet1,
+    minWordLength: 4,
+    maxWordLength: 8,
+    difficulty: 'MEDIUM',
+  })
+
+  const hardSet1 = generate(250, {
+    minLength: 5,
+    maxLength: 8,
+    join: '|',
+  }) as string
+  await prisma.createTrial({
+    name: 'typvp Hard Trial 1',
+    wordSet: hardSet1,
+    minWordLength: 5,
+    maxWordLength: 8,
+    difficulty: 'HARD',
   })
 }
 

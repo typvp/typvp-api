e9f1e8b361e29e70c0426e4a3f4213452cd10ffb
Evan Kysley <evan@kysley.com>
2019-12-05 16:38:20 -0500

    feat: improve races

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {GraphQLSchema} from 'graphql'
 import Redis from 'ioredis'
 import io, {Socket} from 'socket.io'
 import http from 'http'
+import uuid from 'uuid'
 
 import {prisma} from './generated/prisma-client'
 import {AuthorizationCheck} from './middleware/Auth'
@@ -19,29 +20,24 @@ export const redis = new Redis(process.env.REDIS_PORT, {
   password: process.env.REDIS_PASSWORD,
 })
 
-const rooms = {
-  room1: {
-    countdown: null,
-    secondsRemaining: 60,
-    acceptUpdates: false,
-    name: 'room1',
-    state: 'waiting',
-    players: {},
+const app = express()
+
+const socketServer = http.createServer(app)
+socketServer.listen(8086)
+const ios = io(socketServer, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
+      'Access-Control-Allow-Credentials': true,
+    }
+    res.writeHead(200, headers)
+    res.end()
   },
-}
+})
 
-async function bootstrap() {
-  const schema = (await buildSchema({
-    resolvers: [TrialResolver, AccountResolver, TestResolver],
-    authChecker: AuthorizationCheck,
-    dateScalarMode: 'isoDate',
-  }).catch(e => {
-    console.log(e)
-  })) as GraphQLSchema
-
-  const app = express()
+async function initExpress() {
   app.disable('x-powered-by')
-
   app.get('/confirm/:id', async (req, res) => {
     const {id} = req.params
     const userId = await redis.get(id)
@@ -66,87 +62,103 @@ async function bootstrap() {
       res.send('It looks like your email validation link expired...')
     }
   })
+}
 
-  const socketServer = http.createServer(app)
-  socketServer.listen(8086)
-  const ios = io(socketServer, {
-    handlePreflightRequest: (req, res) => {
-      const headers = {
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
-        'Access-Control-Allow-Credentials': true,
-      }
-      res.writeHead(200, headers)
-      res.end()
-    },
-  })
+type TRoom = {
+  countdown: number
+  secondsRemaining: number
+  acceptUpdates: boolean
+  name: string
+  state: string
+  players: any
+}
 
-  ios.on('connection', (socket: Socket) => {
-    let playerID: string
-    let sendUpdate: boolean = false
-    console.log('connection')
+interface IRoom {
+  [key: string]: TRoom
+}
 
-    socket.on('race-matchmake', async data => {
-      // const rooms = await redis.scan(0, 'MATCH', 'room:*')
-      console.log(data)
-      const {id} = data
-      playerID = id
-      socket.join('room1')
-      rooms.room1.players = {...rooms.room1.players, [playerID]: 0}
-      if (Object.keys(rooms.room1.players).length === 2) {
-        rooms.room1.state = 'starting'
-        let seconds = 5
-        const countdown = setInterval(() => {
-          if (seconds === 0) {
-            rooms.room1.countdown = 0
-            rooms.room1.state = 'in-progress'
-            rooms.room1.acceptUpdates = true
-            const duration = setInterval(() => {
-              rooms.room1.secondsRemaining -= 1
-              ios.to('room1').emit('request-race-progress', rooms.room1)
-            }, 1000)
-            setTimeout(() => {
-              rooms.room1.state = 'finished'
-              rooms.room1.acceptUpdates = false
-              ios.to('room1').emit('update', rooms.room1)
-              ios.to('room1').removeAllListeners('room1')
-              clearInterval(duration)
-            }, 60000)
-            clearInterval(countdown)
-            ios.to('room1').emit('update', rooms.room1)
-          } else {
-            rooms.room1.countdown = seconds
-            seconds--
-            ios.to('room1').emit('update', rooms.room1)
-          }
-        }, 1000)
-      }
+let rooms: IRoom
 
-      ios.to('room1').emit('update', rooms.room1)
+const PLAYERS_PER_RACE = 2
+const RACE_DURATION = 60000
+
+async function processQueue() {
+  const queueLength = await redis.llen('queue')
+  console.log(`${queueLength} players in queue `)
+  if (queueLength >= PLAYERS_PER_RACE) {
+    const players = await redis.lrange('queue', 0, PLAYERS_PER_RACE - 1)
+    console.log(players)
+    let playerList = {}
+    players.forEach(async (player: string) => {
+      const playerId = await redis.hget(player, 'id')
+      playerList = {...playerList, [playerId]: 0}
     })
+    const roomUUID = uuid()
+    rooms = {
+      ...rooms,
+      [roomUUID]: {
+        countdown: null,
+        secondsRemaining: 60,
+        acceptUpdates: false,
+        name: `room_${roomUUID}`,
+        state: 'waiting',
+        players: playerList,
+      },
+    }
+    players.forEach(async (player: string) => {
+      await redis.hset(player, 'roomId', roomUUID)
+      ios.sockets.sockets[player].join(`room_${roomUUID}`)
+    })
+    await redis.ltrim('queue', PLAYERS_PER_RACE, -1)
+    startRoom(roomUUID)
+  }
+}
 
-    socket.on('race-progress', data => {
-      console.log(data)
-      rooms.room1.players[playerID] = data.cpm
-      if (sendUpdate) {
-        ios.to('room1').emit('update', rooms.room1)
-        sendUpdate = false
-      }
-      setTimeout(() => {
-        sendUpdate = true
+async function startRoom(roomUUID: string) {
+  rooms[roomUUID].state = 'starting'
+  let seconds = 5
+  const countdown = setInterval(() => {
+    if (seconds === 0) {
+      rooms[roomUUID].countdown = 0
+      rooms[roomUUID].state = 'in-progress'
+      rooms[roomUUID].acceptUpdates = true
+      const duration = setInterval(() => {
+        rooms[roomUUID].secondsRemaining -= 1
+        ios
+          .to(`room_${roomUUID}`)
+          .emit('race_request-progress', rooms[roomUUID])
       }, 1000)
-    })
+      setTimeout(() => {
+        rooms[roomUUID].state = 'finished'
+        rooms[roomUUID].acceptUpdates = false
+        ios.to(`room_${roomUUID}`).emit('update', rooms[roomUUID])
+        ios.to(`room_${roomUUID}`).clients((_: any, clients: [string]) => {
+          clients.forEach(async socketId => {
+            await redis.hdel(socketId, 'roomId')
+            ios.sockets.sockets[socketId].leave(`room_${roomUUID}`)
+          })
+        })
+        delete rooms[roomUUID]
+        clearInterval(duration)
+      }, RACE_DURATION)
+      clearInterval(countdown)
+      ios.to(`room_${roomUUID}`).emit('update', rooms[roomUUID])
+    } else {
+      rooms[roomUUID].countdown = seconds
+      seconds--
+      ios.to(`room_${roomUUID}`).emit('update', rooms[roomUUID])
+    }
+  }, 1000)
+}
 
-    socket.on('disconnect', () => {
-      console.log('disconnected')
-      // const newPlayers = rooms.room1.players.filter(
-      //   player => player.playerID !== playerID,
-      // )
-      delete rooms.room1.players[playerID]
-      // rooms.room1.players = [...newPlayers]
-      ios.to('room1').emit('update', rooms.room1)
-    })
-  })
+async function initGraphQL() {
+  const schema = (await buildSchema({
+    resolvers: [TrialResolver, AccountResolver, TestResolver],
+    authChecker: AuthorizationCheck,
+    dateScalarMode: 'isoDate',
+  }).catch(e => {
+    console.log(e)
+  })) as GraphQLSchema
 
   const server = new ApolloServer({
     subscriptions: {
@@ -182,6 +194,53 @@ async function bootstrap() {
       `typvp-api has started at http://localhost:${process.env.PORT}/graphql`,
     ),
   )
+}
+
+async function initSocketIO() {
+  ios.on('connection', async (socket: Socket) => {
+    let playerID: string
+    let sendUpdate: boolean = false
+    console.log('connection')
+
+    socket.on('race_queue', async data => {
+      playerID = data.id
+      await redis.hset(socket.id, 'id', data.id)
+      await redis.rpush('queue', socket.id)
+      processQueue()
+    })
+
+    socket.on('race_progress', async data => {
+      const info = await redis.hgetall(socket.id)
+      rooms[info.roomId].players[info.id] = data.cpm
+      if (sendUpdate) {
+        ios.to(info.roomId).emit('update', rooms[info.roomId])
+        sendUpdate = false
+      }
+      setTimeout(() => {
+        sendUpdate = true
+      }, 1000)
+    })
+
+    socket.on('disconnect', async () => {
+      console.log('disconnected')
+      // const newPlayers = rooms.room1.players.filter(
+      //   player => player.playerID !== playerID,
+      // )
+      // delete rooms.room1.players[playerID]
+      // rooms.room1.players = [...newPlayers]
+      // ios.to('room1').emit('update', rooms.room1)
+      await redis.del(socket.id)
+      await redis.lrem('queue', 0, socket.id)
+      socket.leaveAll()
+      socket.removeAllListeners()
+    })
+  })
+}
+
+async function bootstrap() {
+  await initExpress()
+  await initSocketIO()
+  await initGraphQL()
 }
 
 bootstrap()

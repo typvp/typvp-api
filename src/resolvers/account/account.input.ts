7c7b11c6912635b261abd3dfd7c92eaa91301992
Evan Kysley <evan@kysley.com>
2020-01-09 13:00:05 -0500

    feat: field validation on input

--- src/resolvers/account/account.input.ts
+++ src/resolvers/account/account.input.ts
@@ -1,14 +1,22 @@
 import {Field, InputType} from 'type-graphql'
+import {IsEmail, IsString, Max, Min} from 'class-validator'
 
 @InputType()
 export class AccountSignupInput {
   @Field(type => String)
+  @IsEmail()
   email: string
 
   @Field(type => String)
+  @IsString()
+  @Min(5)
+  @Max(25)
   password: string
 
   @Field(type => String)
+  @IsString()
+  @Min(3)
+  @Max(15)
   username: string
 }
 

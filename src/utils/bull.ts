5cff1b7587a825c05a61dbe04fe2d2011c1bfc3e
Evan Kysley <evan@kysley.com>
2020-03-06 16:43:08 -0500

    refactor: update queue name

--- src/utils/bull.ts
+++ src/utils/bull.ts
@@ -1,6 +1,6 @@
 import Queue from 'bull'
 
-export const confirmEmailQueue = new Queue('confirm-email', {
+export const emailQueue = new Queue('email', {
   redis: {
     port: process.env.REDIS_PORT as any,
     password: process.env.REDIS_PASSWORD as string,
